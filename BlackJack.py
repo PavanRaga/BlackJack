8aca3365c026cbf467d0d59a4a72096ef6e39267
Pavan R <pavan.raga98@gmail.com>
2020-05-19 18:18:56 +0800

    Update BlackJack.py

--- BlackJack.py
+++ BlackJack.py
@@ -10,7 +10,6 @@
 9. Check if 21
 10. Check if more than 21
 11 if stay, hit dealer till he beats the player or goes above 21.
-
 Classes:
 1. Dealer
 attr: Deck
@@ -36,15 +35,16 @@ method:
 '''
 import random
 from time import sleep
+from collections import defaultdict
 
 cards = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10,
          'Queen':10, 'King':10, 'Ace':11}
 
-deck = {}
-deck['Hearts'] = cards;
-deck['Diamonds'] = cards;
-deck['Spades'] = cards;
-deck['Clubs'] = cards;1
+deck = defaultdict(dict)
+deck['Hearts'] = cards
+deck['Diamonds'] = cards
+deck['Spades'] = cards
+deck['Clubs'] = cards
 
 class deck_class():
     def __init__(self, init_deck):
@@ -55,9 +55,6 @@ class deck_class():
     def pop(self):
         self.suit, self.cards = random.choice(list(self.deck.items()))
         self.card = random.choice(list(self.cards.items()))
-        #print(self.card)
-        #del self.deck[self.suit][self.card[0]]
-        #print(self.deck)
         return (self.suit, self.card)
 
 class player():
@@ -98,10 +95,10 @@ class dealer():
         return (card1,card2,card3,card4)
     def check21(self,cards):
         if cards == 21:
-            self.winner('player',cards)
+            self.winner('\t player',cards)
             return 1
         elif cards > 21:
-            self.lose('player',cards)
+            self.lose('\t player',cards)
             return 1
         else:
             return 0
@@ -125,6 +122,7 @@ class dealer():
 
 play = 1
 min_bet = 10
+win = False
 
 print("Welcome to BlackJack!")
 
@@ -140,16 +138,14 @@ while(play):
     if not player1.pay(bet):
         print("Looks like you dont have enough chips to play!")
         continue
-    print("Dealing..")
+    print("Dealing..", end="")
     sleep(0.5)
     print("..")
-    sleep(0.9)
-    print(".")
     card = dealerman.deal()
     #print(card)
     dealerman.cardss(card[0],card[1])
     player1.cardss(card[2],card[3])
-    print("Dealer's one card value: {}".format(card[2][1][1]))
+    print("Dealer's one card value: {}".format(card[0][1][1]))
     if dealerman.check21(player1.get_card_value()):
         pass
     else:
@@ -168,18 +164,14 @@ while(play):
                 else:
                     continue
         #player1 done, dealer
-        print(game_over)
+        # print(game_over)
+        # means player chose to stop the hit before game over
         if not game_over:
             while(dealerman.get_card_value() < 21):
                 dealerman.cardss(dealerman.hit())
                 print("Dealer's card value after hit {}".format(dealerman.get_card_value()))
                 if (dealerman.get_card_value() <= 21 and dealerman.get_card_value() > player1.get_card_value()):
-                    dealerman.winner('dealer', dealerman.get_card_value())
-                    break
-            dealerman.winner('player1', player1.get_card_value())
-
-
-
-
-
-
+                    dealerman.winner('\t Dealer', dealerman.get_card_value())
+                    win = True
+            if not win:
+                dealerman.winner('\t player1', player1.get_card_value())
